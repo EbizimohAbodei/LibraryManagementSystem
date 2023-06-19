b91855723b328b98d4c0a9575a62386b790d7f0b
kendyvincent <vincentkendy@gmail.com>
2023-06-19 08:48:14 +0300

    Moved person and book class to shared folder

--- src/Shared/BorrowableBook.cs
+++ src/Shared/BorrowableBook.cs
@@ -1,22 +1,22 @@
-// using System;
-// using System.Collections;
+using System;
+using System.Collections;
 
-// namespace LibrarySystem;
+namespace LibrarySystem;
 
-// class BorrowableBook : Book, IBorrowable
-// {
-//     public BorrowableBook(string title, string author, string isbn, int publicationYear)
-//         : base(title, author, isbn, publicationYear)
-//     {
-//     }
+class BorrowableBook : Book, IBorrowable
+{
+    public BorrowableBook(string title, string author, string isbn, int publicationYear)
+        : base(title, author, isbn, publicationYear)
+    {
+    }
 
-//     public void Borrow()
-//     {
-//         Console.WriteLine($"Book '{Title}' has been borrowed.");
-//     }
+    public void Borrow()
+    {
+        Console.WriteLine($"Book '{Title}' has been borrowed.");
+    }
 
-//     public void Return()
-//     {
-//         Console.WriteLine($"Book '{Title}' has been returned.");
-//     }
-// }
+    public void Return()
+    {
+        Console.WriteLine($"Book '{Title}' has been returned.");
+    }
+}
